303adf8b454b127bb9fc1729463944a85712ae7a
Matt <johnmjones@valdosta.edu>
2015-09-01 22:01:36 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(require('./router'));
+
+app.listen(18881, function() {
+    console.log('ready on port 18881');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text-plain'});
+    res.end('Hello World\n');
+}).listen(18881, 'localhost');
+console.log("Server running at localhost:18881");
+    */
